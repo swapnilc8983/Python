9a58c431b21c6c6d19e41357735123e072f716dd
swapnil chavan <50729814+swapnilc8983@users.noreply.github.com>
2019-11-30 13:56:39 +0530

    Code For Spin Content Using Google translate

---
+++ spin-content/spincontent.py
@@ -0,0 +1,22 @@
+# built-in function 
+# (pip install googletrasns) Cammand for install google trasnslate 
+
+
+
+from googletrans import Translator # Import Translator 
+translator = Translator() # Set As Variable 
+def Spin(abc): 
+    a = translator.translate(abc, dest = 'en') # dest = Language to transale & abc is Content 
+    b = translator.translate(a.text, dest = 'fr') # a.text for fetch text 
+    c = translator.translate(b.text, dest = 'de')
+    d = translator.translate(c.text, dest = 'it')
+    e = translator.translate(d.text, dest = 'es')
+    f = translator.translate(e.text, dest = 'en')
+    print("Spin Text : " + f.text) # final conetent print
+
+text = input("Enter Text: ")
+
+Spin(text)
+
+
+
