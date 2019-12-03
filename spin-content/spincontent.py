1ca520b209400255ec4a0b2f73c6ba4ea7ae204d
swapnil chavan <50729814+swapnilc8983@users.noreply.github.com>
2019-12-03 10:00:12 +0530

    Using File handling

--- spin-content/spincontent.py
+++ spin-content/spincontent.py
@@ -3,18 +3,22 @@
 
 from googletrans import Translator # Import Translator 
 translator = Translator() # Set As Variable 
-def Spin(abc): 
+def Spin(abc):  
+    file = open('spincontent.txt','w') # to Save Spin Conetent in spincontent.txt file
     a = translator.translate(abc, dest = 'en') # dest = Language to transale & abc is Content ex: en = english
     b = translator.translate(a.text, dest = 'fr') # a.text for fetch only text 
     c = translator.translate(b.text, dest = 'de')
     d = translator.translate(c.text, dest = 'it')
     e = translator.translate(d.text, dest = 'es')
     f = translator.translate(e.text, dest = 'en')
-    print("Spin Text : " + f.text) # final content print
+    return file.write(f.text) # Output Save
+    #print("Spin Text : " + f.text) # final content print
 
-text = input("Enter Text: ")
+#text = input("Enter Text: ")
 
-Spin(text)
+#Spin(text)
 
-
+file = open('content.txt','r') # fro read a original content
+Spin(file.read()) # Call spin function
+file.close() # file close
 
