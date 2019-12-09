649e39e60d2f020a09eceb0503243c7e6394336e
swapnil chavan <50729814+swapnilc8983@users.noreply.github.com>
2019-12-09 11:34:37 +0530

    Calculate Employee Salary

---
+++ assigmenent/person_salary.py
@@ -0,0 +1,28 @@
+def personSalary(salary, grade):
+        hra = (20/100 * salary)
+        da = (50/100 * salary)
+        if grade == 'A':
+            allow = 1700
+        elif grade == 'B':
+            allow = 1500
+        else:
+            allow = 1300
+        
+        pf = (11/100 * salary)          
+        
+        totalsalary = int( salary + hra + da + allow - pf)
+        print("Total Salary: {}" .format(totalsalary)) 
+
+salary = int(input("Basic Salary : "))
+grade = str(input("Grade: ").capitalize())
+
+personSalary(salary, grade)
+
+------------
+#Input: 
+#Basic Salary : 10000
+#Grade: a
+--------------
+#output: 
+#Total Salary: 17600
+------------
