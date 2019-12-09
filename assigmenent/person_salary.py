717f09fe6cc24a999d03c25e9a5a460b28a7ecc3
swapnil chavan <50729814+swapnilc8983@users.noreply.github.com>
2019-12-09 12:27:30 +0530

    Update person_salary.py

--- assigmenent/person_salary.py
+++ assigmenent/person_salary.py
@@ -1,20 +1,22 @@
 def personSalary(salary, grade):
         hra = (20/100 * salary)
         da = (50/100 * salary)
-        if grade == 'A':
+        if grade == 'A' or 'a':
             allow = 1700
-        elif grade == 'B':
+        elif grade == 'B' or 'b':
             allow = 1500
         else:
             allow = 1300
         
         pf = (11/100 * salary)          
         
-        totalsalary = int( salary + hra + da + allow - pf))
+        totalsalary = float( salary + hra + da + allow - pf)
+        roundSalary = int(round(totalsalary))
         print("Total Salary: {}" .format(totalsalary)) 
+        print("Round off Salary is: {}" .format(roundSalary))
 
-salary = round(float(input("Basic Salary : ")))
-grade = str(input("Grade: ").capitalize())
+salary = float(input("Basic Salary : "))
+grade = str(input("Grade: "))
 
 personSalary(salary, grade)
 
